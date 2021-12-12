f31df6ef2c35cc28b87558ac0d5c5f3977e405f7
techin <techin.won@innosoft.kmutt.ac.th>
2021-12-12 21:26:38 +0700

    Create EmailAllController
    Create EmailAllController

--- app/Http/Controllers/API/EmailAllController.php
+++ app/Http/Controllers/API/EmailAllController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MailAll;
 
 class EmailAllController extends Controller
 {
-    //
+    public function getEmailAll(){
+        return MailAll::all();
+    }
 }
