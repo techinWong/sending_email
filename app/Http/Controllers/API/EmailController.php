b6ef9aea1ee53daaac02d246eaacb7fe6a44402b
techin <techin.won@innosoft.kmutt.ac.th>
2021-12-12 21:29:59 +0700

    Delete EmailAllController
    Delete EmailAllController

--- app/Http/Controllers/API/EmailController.php
+++ app/Http/Controllers/API/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\MailSender;
+use App\Models\MailAll;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
@@ -14,5 +15,9 @@ class EmailController extends Controller
 
     public function getEmailSender(){
         return MailSender::all();
+    }
+
+    public function getEmailAll(){
+        return MailAll::all();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\EmailController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('mailsender',[EmailController::class,'getEmailSender']);
+
+Route::get('mail/all',[EmailController::class,'getEmailAll']);
