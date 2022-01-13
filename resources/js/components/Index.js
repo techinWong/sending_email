c423e0d7550f4a04a0ed9bf15bda6fa77f626aaf
techin <techin.won@innosoft.kmutt.ac.th>
2022-01-13 17:54:29 +0700

    latest file
    latest file

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -129,26 +129,7 @@ const Index = () => {
         <div className="container">
             <div className="row justify-content-center">
 
-            {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Email Form</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className="collapse navbar-collapse show" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            <a className="nav-link" href="/history">History</a>
-                            <a className="nav-link" href="/template">Template</a>
-                         </div>
-                        </div>
-                     </div>
-            </nav> */}
             <Navbar Home='active'/>
-
-            
-
-
 
                 <div className="col-md-8">
                     <div className="card">
--- resources/js/components/SaveTemplate.js
+++ resources/js/components/SaveTemplate.js
@@ -53,25 +53,7 @@ const SaveTemplate = () => {
         <div className="container">
             <div className="row justify-content-center">
 
-            {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Email Form</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className="collapse navbar-collapse show" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-link" aria-current="page" href="http://127.0.0.1:8000/">Home</a>
-                            <a className="nav-link" href="/history">History</a>
-                            <a className="nav-link active" href="/template">Template</a>
-                         </div>
-                        </div>
-                     </div>
-            </nav> */}
-
             <Navbar Template='active'/>
-
-            
 
             <div className="col-md-8">
                     <div className="card">
